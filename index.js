686b1bd95fcee1e2673eba0708b45f470d658b53
Julius Breckel <julius.breckel@signavio.com>
2017-03-08 16:20:33 +0300

    add jest rules

--- index.js
+++ index.js
@@ -3,12 +3,16 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'no-unused-expressions': 'off',
+
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
   },
   plugins: [
     'jest',
   ],
   env: {
     mocha: true,
-    jest: true,
+    'jest/globals': true,
   },
 }
